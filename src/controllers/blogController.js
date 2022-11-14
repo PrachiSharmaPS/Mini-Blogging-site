17fc105d417b26abbe7e598a4950d33fd17cd1b8
sunandasharma61 <sunandasharma60101@gmail.com>
2022-11-14 17:09:16 +0530

    blog controller

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,7 +1,34 @@
-const { create } = require('../models/authorModel')
+const { isValidObjectId } = require('mongoose');
+const authorModel= require('../models/authorModel')
 const blogModel = require('../models/blogModel')
 
-const populate = populate('authorId')
 
+const createBlog = async function(req,res){
+    try{
+        let blog=req.body;
+        let{title,body,authorId,category}= blog;
+        if(!title){
+            return res.status(400).send({status:false,msg:'title is mandatory'});
+        }
+        if(!body){
+            return res.status(400).send({status:false,msg:'body is mandatory'});
+        }
+        if(!authorId){
+            return res.status(400).send({status:false,msg:'author id  is a required field'});
+        }
+        if(!category){
+            return res.status(400).send({status:false,msg:'category is mandatory'});
+        }
+        if(!isValidObjectId(authorId)){
+            return res.status(400).send({status:false,msg:'auhtor id is not valid'});
+        }
+    let blogData=await blogModel.create(blog);
+    return res.status(201).send({status:true,msg:'blog has been created'});
+  }
+  catch(err){
+    return res.status(500).send({status:false,msg:err.message});
+  }
+};
 
+module.exports.createBlog=createBlog;
 
