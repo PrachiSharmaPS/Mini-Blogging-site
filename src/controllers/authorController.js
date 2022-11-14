1734e20e345cf810c8e3fe15891fa4e78f30e93c
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2022-11-14 17:35:50 +0530

    author controller

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,28 +1,28 @@
 const authorModel = require('../models/authorModel')
 
-let regex=new RegExp('[a-z0-9]+@[a-z].com');
+let regex=new RegExp('[a-z0-9]+@[a-z]mail.com');
 
 const createAuthor= async function (req, res) {
     try{
     let author = req.body
-    let{title,fname,lname,email,password}=author.email
-    if(!title){ res.status(400).send({status:false,msg :"title is needed"})}
-    if(!fname){ res.status(400).send({status:false,msg :" first name is needed"})}
-    if(!lname){ res.status(400).send({status:false,msg :"last name is needed"})}
-    if(!email){ res.status(400).send({status:false,msg :"email is needed"})}
-    if(!password){ res.status(400).send({status:false,msg :"passward is needed"})}
+    let{title,fname,lname,email,password}=author
+    if(!title){return res.status(400).send({status:false,msg :"title is needed"})}
+    if(!fname){return res.status(400).send({status:false,msg :" first name is needed"})}
+    if(!lname){return res.status(400).send({status:false,msg :"last name is needed"})}
+    if(!email){return res.status(400).send({status:false,msg :"email is needed"})}
+    if(!password){return res.status(400).send({status:false,msg :"passward is needed"})}
     
 
     let verifyEmail= regex.test(email)
-    if(!verifyEmail){ res.status(400).send({status:false,msg :"invalid email"})}
+    if(!verifyEmail){ return res.status(400).send({status:false,msg :"invalid email"})}
 
 
     let authorCreated = await authorModel.create(author)
-    res.status(201).send({status:true,data: authorCreated})
+    return res.status(201).send({status:true,data: authorCreated})
     }
     catch(err) {
         console.log("this is error:",err.message)
-        res.status(500).send({status:false,msg:err})
+       return res.status(500).send({status:false,msg:err})
     }
 }
 
