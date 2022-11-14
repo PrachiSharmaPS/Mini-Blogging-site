a3c7c9c1ec6c76b0ec8e8d2ea6b08453d04b9c50
sunandasharma61 <sunandasharma60101@gmail.com>
2022-11-14 15:01:49 +0530

    commiting all code

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+const route=require('../routes/route.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+mongoose.connect("mongodb+srv://userProject1:WlhZV1CHyTcTiSlZ@blog.osplkog.mongodb.net/project1", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/',route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
+
---
+++ src/models/blogModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const objectId = mongoose.Schema.type.objectId
+const blogSchema = new mongoose.Schema( {
+    title: {
+        type: String,
+        required:true
+    },
+    body: {
+        type:String,
+        required:true
+    },
+    authorId: {
+        type:objectId,
+        ref:authorModel,
+        required:true
+    },
+    tags: {
+        type:Array,  
+    },
+     category:{
+         type:String,
+         required:true
+     },
+     subcategory:{
+        type:Array
+     },
+     deletedAt:{
+        type:Date
+     },
+      isDeleted: {
+        type:Boolean,
+        default: false
+      },
+       publishedAt: {
+        type:Date
+       }, 
+       isPublished: {
+        type:Boolean,
+        default: false
+    },
+},
+ { timestamps: true });
+
+module.exports = mongoose.model('blog', blogSchema)
