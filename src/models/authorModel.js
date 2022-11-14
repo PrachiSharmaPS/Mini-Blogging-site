bbae3df0c11cc89a6a6583765976b15c3ada29a1
gauravgd9719 <gd220310@gmail.com>
2022-11-14 15:14:19 +0530

    add authorModel.js

---
+++ src/models/authorModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const authorSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    title:{
+        type: String,
+        requried: true,
+        enum: ['Mr', 'Mrs', 'Miss']
+    },
+    email: {
+        type: String,
+        requried: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{ timestamps: true })
+
+module.exports  = mongoose.model('author', authorSchema)
